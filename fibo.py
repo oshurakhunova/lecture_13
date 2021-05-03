2a7d4a615d04b716a37cd04060bde15f56926cd2
Aizhan Oshurakhunova <xoshur00@vutbr.cz>
2021-05-03 10:48:05 +0200

    Add fib. posl

--- fibo.py
+++ fibo.py
@@ -1,6 +1,20 @@
+def rec(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return rec(n - 1) + rec(n - 2)
+
 
 def main():
-    pass
+    n = int(input('Zadejte pocet prvku Fib. posl.: '))
+    fib_seq = []
+    # for num in range(n + 1):
+    #     fib_seq.append(rec(num))
+
+    fib_seq = [rec(num) for num in range(n + 1)]
+    print(fib_seq)
 
 
 if __name__ == '__main__':
