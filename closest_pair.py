f0a5f786603857726b15f291389cd565b9405278
Aizhan Oshurakhunova <xoshur00@vutbr.cz>
2021-05-03 17:40:26 +0200

    add closest pair

--- closest_pair.py
+++ closest_pair.py
@@ -1,6 +1,7 @@
 import os
 import csv
 import matplotlib.pyplot as plt
+import math
 
 cwd_path = os.getcwd()
 file_path = 'files'
@@ -40,7 +41,8 @@ def draw_data(data_points, closest_pair=[]):
         color=['blue' if point not in closest_pair else 'red' for point in data_points]
     )
     plt.show()
-    
+
+
 def closest_pair_BF(array):
     min_dist = math.dist(array[0], array[1])
     point_1 = array[0]
@@ -59,12 +61,76 @@ def closest_pair_BF(array):
     return point_1, point_2, min_dist
 
 
+def closest_split_pair(x_sorted, y_sorted, min_dist, points):
+    num_points = len(x_sorted)
+    midpoint_x = x_sorted[num_points // 2][0]
+
+    subarray_y = []
+    for point in y_sorted:
+        if midpoint_x - min_dist <= point[0] <= midpoint_x + min_dist:
+            subarray_y.append(point)
+
+    min_dist_new = min_dist
+    len_y = len(subarray_y)
+    for i in range(len_y - 1):
+        for j in range(i + 1, min(i + 7, len_y)):
+            point_1, point_2 = subarray_y[i], subarray_y[j]
+            dst = math. dist(point_1, point_2)
+            if dst < min_dist_new:
+                points = point_1, point_2
+                min_dist_new = dst
+    return points[0], points[1], min_dist_new
+
+
+def closest_pair(x_sorted, y_sorted):
+    num_points = len(x_sorted)
+    mid_idx = num_points // 2
+
+    if num_points <= 3:
+        point_1, point_2, min_dist = closest_pair_BF(x_sorted)
+        return point_1, point_2, min_dist
+
+    left_x = x_sorted[:mid_idx]
+    right_x = x_sorted[mid_idx:]
+
+    left_y = []
+    right_y = []
+    for point in y_sorted:
+        if point[0] < x_sorted[mid_idx][0]:
+            left_y.append(point)
+        else:
+            right_y.append(point)
+
+    p1, q1, min_dist_1 = closest_pair(left_x, left_y)
+    p2, q2, min_dist_2 = closest_pair(right_x, right_y)
+
+    if min_dist_1 <= min_dist_2:
+        min_dist = min_dist_1
+        points = (p1, q1)
+    else:
+        min_dist = min_dist_2
+        points = (p2, q2)
+
+    p3, q3, min_dist_3 = closest_split_pair(x_sorted, y_sorted, min_dist, points)
+
+    if min_dist <= min_dist_3:
+        return points[0], points[1], min_dist
+    else:
+        return p3, q3, min_dist_3
+
+
 def main(file_name):
     # read data points
     data_points = read_file(file_name)
 
+    # p1, p2, dist = closest_pair_BF(data_points)
+
+    x_sorted = sorted(data_points, key=lambda x: x[0])
+    y_sorted = sorted(data_points, key=lambda x: x[1])
+    p1, p2, dist = closest_pair(x_sorted, y_sorted)
+
     # draw points
-    draw_data(data_points)
+    draw_data(data_points, [p1, p2])
 
 
 if __name__ == '__main__':
